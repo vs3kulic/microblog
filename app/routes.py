6b80873e91daf7d89735284644063d8a4249abc1
vsekulic <sekulicvajo@gmail.com>
2021-08-05 21:42:41 +0200

    pw hashing, flask-login, user loader, login, logout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User 
 
 @app.route('/')
 @app.route('/index')
@@ -22,26 +24,28 @@ def index():
             'author': {'username': 'KasperSchmeichel'},
             'body': 'The laser was no factor at all!'
         },
-        {
-            'author': {'username': 'JohnWick'},
-            'body': 'The weather in Rome is wicked!'
-        },
-        {
-            'author': {'username': 'LenaHeadey'},
-            'body': 'The love movie was so sweet!'
-        },
-        {
-            'author': {'username': 'KasperSchmeichel'},
-            'body': 'The laser was no factor at all!'
-        }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	
+	if current_user.is_authenticated: 
+		return redirect(url_for('index'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    	user = User.query.filter_by(username=form.username.data).first()
+    	if user is None or not user.check_password(form.password.data):
+    		flash('Invalid username or password')
+    		return redirect(url_for('login'))
+    	login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
+
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
