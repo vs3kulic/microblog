f40a1f2eeebc5ce15e404c47435a501902555a39
vsekulic <sekulicvajo@gmail.com>
2021-08-07 07:53:18 +0200

    removed user scaffold

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,16 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
-from app.models import User 
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 
 #this is a the main view function 
 def index():
-    user = {'username': 'Vajo'}
 
     posts = [
         {
@@ -25,7 +26,7 @@ def index():
             'body': 'The laser was no factor at all!'
         },
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,17 +34,20 @@ def login():
 	if current_user.is_authenticated: 
 		return redirect(url_for('index'))
 
-    form = LoginForm()
+	form = LoginForm()
+    
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+			login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
 
-    if form.validate_on_submit():
-    	user = User.query.filter_by(username=form.username.data).first()
-    	if user is None or not user.check_password(form.password.data):
-    		flash('Invalid username or password')
-    		return redirect(url_for('login'))
-    	login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-
-    return render_template('login.html', title='Sign In', form=form)
+	return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
