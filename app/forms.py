255a408ba44b7acac2198d593d5110329b45a574
vsekulic <sekulicvajo@gmail.com>
2021-08-09 14:52:20 +0200

    user profile page, avatars and edit profile page added

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User 
 
 class LoginForm(FlaskForm): 
@@ -25,3 +25,8 @@ def validate_email(self, email):
 	user = User.query.filter_by(username=email.data).first()
 	if user is not None: 
 		raise ValidationError('Email in use. Please use a different address.')
+
+class EditProfileForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+	submit = SubmitField('Submit')
