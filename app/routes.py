fd4ba7697e41b7c6a7fde966020b4187b69a9692
vsekulic <sekulicvajo@gmail.com>
2021-08-15 22:47:15 +0200

    post form added

--- app/routes.py
+++ app/routes.py
@@ -3,22 +3,30 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.models import User, Post
 from datetime import datetime
-from app.forms import EmptyForm
+from app.forms import EmptyForm, PostForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
     posts = [
         {
             'author': {'username': 'Kasper'},
             'body': 'Has it ever been home?'
         }
     ]
-    return render_template('index.html', title='Home Page', posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
