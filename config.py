331866c7aef51782b41b19c56286fd7e0bf6e15d
vsekulic <sekulicvajo@gmail.com>
2021-08-24 23:00:18 +0200

    prepared for heroku

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+		'postgres://', 'postgresql://') or \
 		'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -13,4 +14,5 @@ class Config(object):
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['your-email@example.com']
 	POSTS_PER_PAGE=10
+	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
